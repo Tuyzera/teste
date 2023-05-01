2ac7d7e97f07ff1153208c4e25128ad7e75b9529
Arthur Utpadel <utpadelarthur@gmail.com>
2023-05-01 16:53:03 -0300

    Alterações

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function layoutCustom(){
+    document.getElementById('div1').style.backgroundColor = 'red';
+    document.getElementById('body1').style.backgroundColor = 'green';
+    document.getElementById('div1').style.fontFamily = 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+function layoutNormal(){
+    document.getElementById('div1').style.background = 'white';
+    document.getElementById('body1').style.backgroundColor = 'white';
+    document.getElementById('div1').style.fontFamily = 'Verdana', 'Geneva', Tahoma, sans-serif;
+}
