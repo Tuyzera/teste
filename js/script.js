4ecc1e1ec02d9de532c1fb58d51cfd3efcf9b3d7
Arthur Utpadel <utpadelarthur@gmail.com>
2023-05-03 20:47:25 -0300

    Alterações

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-var arr = [];
+const usuarios = JSON.parse(localStorage.getItem("json")) || [];
 
+console.log(usuarios)
+
+
+usuarios.forEach((elemento) =>{
+    console.log(elemento)
+})
 
 function layoutCustom(){
     document.getElementById('div1').style.backgroundColor = 'red';
@@ -13,9 +19,29 @@ function layoutNormal(){
     document.getElementById('div1').style.fontFamily = 'Verdana', 'Geneva', Tahoma, sans-serif;
 }
 
-function addItem(){
-    const usuario = document.getElementById('usuario')
-    const nome = document.getElementById('nome').value;
-    usuario.innerHTML = nome;
- }
+    function addItem(){
+        /* Cria um objeto JavaScript contendo os dados que deseja armazenar. */
+        const json = {
+           nome: document.getElementById('nome').value,
+           senha: document.getElementById('senha').value 
+        }
+
+        criaElemento(json);
+     
+        /* Converta o objeto em uma string JSON usando a função JSON.stringify(). */
+        const userJson = JSON.stringify(json);
+     
+        /* Use a função localStorage.setItem() para armazenar a string JSON em uma chave específica do LocalStorage. */
+        localStorage.setItem('json', userJson);
+     
+
+     }
+
+     function criaElemento(json){
+        const usuario = document.getElementById('usuario')
+       usuario.innerHTML = json.nome;
+
+        
+    }
+
 
